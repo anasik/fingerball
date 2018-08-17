b4ecd3bd581a1eff3ae0911fc237553718321e0f
Anas Ismail Khan <contact@anasismail.com>
2018-08-17 18:09:19 +0500

    So, installed socket and loggd and created a separate file to handle the io events. Now it's time for the real deal.

---
+++ ioevents.js
@@ -0,0 +1,10 @@
+exports = module.exports = function(io){
+
+    io.on('connection', function (socket) {
+        console.log("A user connected.");
+        socket.on('join', function(id) {
+
+        }
+    });
+
+}
