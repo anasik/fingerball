fdba99b74b5baa0c3cd22e865116d9cf83b3604b
Anas Ismail Khan <contact@anasismail.com>
2018-08-19 17:13:55 +0500

    So. I am not sure if I should call it progress. Socketio has a rooms feature which is exactly what I need for this shit. Now I am using mongoose to check the db and do the whole find or create room shit and it also takes care of incrementing the count in the room and of decrementing it when disconnected. However, the same client can theoretically increment twice but decrement only once so I think I should replace the counter with an array containing ids. The length will serve as the counter and the ids will ensure that no one can enter twice. But I still haven't given any real thought to the connection estabilishment protocol and still have no idea how the game works and how the data will be passed.

--- ioevents.js
+++ ioevents.js
@@ -5,23 +5,52 @@ exports = module.exports = function(io, mongoose, db){
     });
     var roomModel = mongoose.model('Room', roomSchema);
     io.on('connection', function (socket) {
-        console.log("A user connected.");
+        console.log("A user connected. Socket ID: "+socket.id);
+        var gameroom;
         socket.on('join', function (id) {
             // Check db to see if the id already exists. If it doesn't create it with 1 person in there.
             // If it does exist already, check if there's room for one person. Else return a message.
 
-            // asd.save(function(err,room){
-            //     if(err)
-            //         console.log("couldnt save "+err);
-            //     });
             roomModel.findById(id, function(err,room){
                 if(err)
                     console.log("find "+err);
-                if(room)
-                    console.log(room._id);
-                else
-                    console.log("Not found");
+                if(room){
+                    if(room.num !== 2){
+                        socket.join(room._id+"");
+                        console.log("Joined room: "+id);
+                        room.num++;
+                        room.save(function(err,room){
+                            if(err)
+                                console.log("Couldn't increment room.num. Error: "+err);
+                            else
+                                console.log("Room count incremented.");
+                        });
+                        gameroom=room;
+                    }
+                    else
+                        console.log("Room full. Can't connect.");
+                }
+                else {
+                    room = new roomModel({_id:id, num:1});
+                    room.save(function(err,room){
+                        if(err)
+                            console.log("Couldn't create room. Error: "+err);
+                        else
+                            console.log("Room "+id+" didn't exist. Created with one user. Joining.");
+                    });
+                    socket.join(room._id+"");
+                    gameroom=room;
+                }
             })
         });
+        socket.on("disconnect", function(reason){
+            gameroom.num--;
+            if(gameroom.num==0)
+                gameroom.remove();
+            else
+                gameroom.save();
+        });
     });
+
 };
+
